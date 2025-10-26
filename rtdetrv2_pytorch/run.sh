dd67c3c8f7deac354e87dfb3ab587a8aa8f59bd4
Monica908368 <monicamagdy712002@gmail.com>
2025-10-25 21:58:43 -0700

    remove

--- rtdetrv2_pytorch/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export CUDA_VISIBLE_DEVICES=1
-
-torchrun --master_port=9909 --master_addr=localhost --nproc_per_node=1 tools/train.py -c configs/rtdetrv2/config.yml --use-amp --seed=0 -r output/rtdetrv2_r18vd_120e_coco/best.pth --test-only 
